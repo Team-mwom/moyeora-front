8d7e1fbc4358b9474cbbe24becc4c5aaec5152b9
Tae Hyeon Lee <harry7141@gmail.com>
2023-11-12 12:45:06 +0900

    리액트 스프링 연동 List(vo) 받아서 화면에 출력

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,56 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-interface Data {
+
+interface Data{
+  idx: number;
   name: string;
   family: string;
   age: number;
   weight: number;
 }
+
+
 function App() {
-  const [data, setData] =  useState<Data | null>(null);
+  const [data, setData] =  useState<Data[] | null>(null);
+  let str: String;
 
   useEffect(() => {
     axios.get('/test')
-    .then((response) => setData(response.data as Data))
-    .catch((error) => console.log(error));
+    .then((response) => setData(response.data as Data[]))
+      .catch((error) => console.log(error));
+   
   }, []);
 
-  return (
-    <div>
-      {data && (
-        <div>
-          <h1>name: {data.name}</h1>
-          <h1>family: {data.family}</h1>
-          <h1>age: {data.age}</h1>
-          <h1>weight: {data.weight}</h1>
-        </div>
-      )}
-    </div>
-  );
-};
+
+  if (data != null) {
+        return (
+          
+          <div>
+           
+            {
+              data.map(rowData => (
+                <div className="sibal">
+                  <div>idx : {rowData.idx}</div>
+                  <div>age : {rowData.age}</div>
+                  <div>name : {rowData.name}</div>
+                  <div>family : {rowData.family}</div>
+                  <div>weight : {rowData.weight}</div>
+                </div>
+                
+                
+              )
+              )
+            }
+            
+              
+          </div>
+        );
+  } else {
+    return (
+      <div></div>);
+  }
+}
+  
 
 export default App;
