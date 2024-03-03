079dd865101180a370862258ea5a8a01b7e2ff9d
yongminiii <dev.yongmini@gmail.com>
2024-03-03 15:11:50 +0900

    feat. 코드 정리

--- src/router/mainRouters/MoyeoraRouter.tsx
+++ src/router/mainRouters/MoyeoraRouter.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import {Routes } from 'react-router-dom';
 
-//페이지
-
-
-
-
-// 레이아웃
-
-
 function MoyeoraRouter() {
   return (
-
-		  	<Routes>
-			</Routes>
-
+    <Routes>
+    </Routes>
   )
 }
 
