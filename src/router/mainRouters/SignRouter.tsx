c9d39344acd658fe95ef1208c45b5d3565bac984
Yongmini <dev.yongmini@gmail.com>
2024-03-13 22:36:14 +0900

    style. 회원가입 페이지 틀 작업 완료

--- src/router/mainRouters/SignRouter.tsx
+++ src/router/mainRouters/SignRouter.tsx
@@ -2,17 +2,11 @@ import SignUpPage from 'pages/sign/SignUpPage';
 import React from 'react';
 import {Routes, Route } from 'react-router-dom';
 
-//페이지
-
-
-// 레이아웃
-
-
 function SignRouter() {
   return (
 
   <Routes>
-    <Route path="/signup" element={<SignUpPage/>} />
+    <Route path="/signUp" element={<SignUpPage/>} />
   </Routes>
 
   )
