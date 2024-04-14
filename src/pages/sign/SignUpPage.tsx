f9061e786adeccd30441940c237c35a307436464
Tae Hyeon Lee <harry7141@gmail.com>
2024-04-14 15:21:27 +0900

    회원가입 1

--- src/pages/sign/SignUpPage.tsx
+++ src/pages/sign/SignUpPage.tsx
@@ -17,7 +17,18 @@ interface Data {
 	name: String,
 	nickName: String,
 	email: String,
-	kakao:String
+	kakao: String
+	memberInfoEntity: {
+
+		addressNum: String,
+		addressLocation: String,
+		addressDetail: String,
+		phoneFirst: String,
+		phoneMiddle: String,
+		phoneLast: String,
+		gender: String,
+		age:number
+	}
 }
 
 interface CheckData{
@@ -29,7 +40,11 @@ interface CheckData{
 const SignUpPage = () => {
 	const navigate = useNavigate();
 	const [cookies, setCookie, removeCookie] = useCookies(); 
-	const data: Data = { name: '', nickName: '', email: '', kakao: useLocation().state }
+	const data: Data =
+	{
+		name: '', nickName: '', email: '', kakao: useLocation().state, memberInfoEntity: {
+			addressNum:"",addressLocation:"",addressDetail:"",phoneFirst:"",phoneMiddle:"",phoneLast:"",gender:"남",age:0
+		} }
 	const checkData: CheckData = {name:false,nickName:false,email:false}
 	const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
 
@@ -47,9 +62,8 @@ const SignUpPage = () => {
 
 	const signup = useCallback(() => { 
 	
+		
 		axios.post('/api/all/signup', data).then((res) => { //회원가입 정보를 서버로 보내서 디비에 저장한다.
-			
-
 			axios({// 디비 저장에 성공했다면 카카오 아이디로 로그인을 실시 한다.
 				method: "GET",
 				url: `/api/all/signIn?kakaoId=${data.kakao}`,
@@ -119,6 +133,35 @@ const SignUpPage = () => {
 
 	},[]);
 		
+	const checkNumber = useCallback((e: any) => {
+		e.target.value = e.target.value.replace(/[^0-9]/gi, "");
+   },[])
+	const onChangeAge =  useCallback((e: any) => { 
+		data.memberInfoEntity.age = e.target.value;
+	}, []);
+	const onChangeGender =  useCallback((e: any) => { 
+		data.memberInfoEntity.gender = e.target.value;
+	}, []);
+	const onChangePhone1 =  useCallback((e: any) => { 
+		data.memberInfoEntity.phoneFirst = e.target.value;
+	}, []);
+	const onChangePhone2 =  useCallback((e: any) => { 
+		data.memberInfoEntity.phoneMiddle = e.target.value;
+	}, []);
+	const onChangePhone3 =  useCallback((e: any) => { 
+		data.memberInfoEntity.phoneLast = e.target.value;
+	}, []);
+	const onChangeAddressNum =  useCallback((e: any) => { 
+		data.memberInfoEntity.addressNum = e.target.value;
+	}, []);
+	const onChangeAddressLocation =  useCallback((e: any) => { 
+		data.memberInfoEntity.addressLocation = e.target.value;
+	}, []);
+	const onChangeAddressDetail =  useCallback((e: any) => { 
+		data.memberInfoEntity.addressDetail = e.target.value;
+	}, []);
+
+
 
 
 	return (
@@ -136,8 +179,38 @@ const SignUpPage = () => {
 								aria-label="Small"
 								aria-describedby="inputGroup-sizing-sm"
 								onChange={onChangeName}
+							/>
+								<InputGroup.Text id="inputGroup-sizing-sm" >나이</InputGroup.Text>
+							<Form.Control
+								type="number"
+								min='7' max='100'
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onKeyUp={checkNumber}
+								onChange={onChangeAge}
 								/>
+							<InputGroup.Text id="inputGroup-sizing-sm" >성별</InputGroup.Text>
+				
+								<Form.Check
+									inline
+									label="남"
+									name="group1"
+									type='radio'
+									value={"남"}
+								onChange={onChangeGender}
+								defaultChecked
+								/>
+								<Form.Check
+									inline
+									label="여"
+									name="group1"
+									type='radio'
+									value={"여"}
+									onChange={onChangeGender}
+								/>
+						
 						</InputGroup>
+			
 						<InputGroup size="sm" className="mb-3">
 							<InputGroup.Text id="inputGroup-sizing-sm">닉네임</InputGroup.Text>	
 							<Form.Control
@@ -155,6 +228,50 @@ const SignUpPage = () => {
 								onChange={onChangeEmail}
 								/>
 						</InputGroup>
+						<InputGroup size="sm" className="mb-3">
+							<InputGroup.Text id="inputGroup-sizing-sm">휴대폰</InputGroup.Text>
+							<Form.Control
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onChange={onChangePhone1}
+							/>
+							<InputGroup.Text id="inputGroup-sizing-sm">-</InputGroup.Text>
+							<Form.Control
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onChange={onChangePhone2}
+							/>
+							<InputGroup.Text id="inputGroup-sizing-sm">-</InputGroup.Text>
+							<Form.Control
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onChange={onChangePhone3}
+								/>
+						</InputGroup>
+						<InputGroup size="sm" className="mb-3">
+							<InputGroup.Text id="inputGroup-sizing-sm">주소</InputGroup.Text>
+							<Form.Control
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onChange={onChangeAddressLocation}
+							/><InputGroup.Text id="inputGroup-sizing-sm">우편번호</InputGroup.Text>
+							<Form.Control
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onChange={onChangeAddressNum}
+							/>
+							<Button variant="dark" >주소찾기</Button>
+						</InputGroup>
+							<InputGroup size="sm" className="mb-3">
+							<InputGroup.Text id="inputGroup-sizing-sm">상세주소</InputGroup.Text>
+							<Form.Control
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								onChange={onChangeAddressDetail}
+							/>
+							
+						</InputGroup>
+
 
 						
 					</div>
