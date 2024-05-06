8bd645ec4f5d98869217c353a188c66da6dd3a84
Tae Hyeon Lee <harry7141@gmail.com>
2024-05-06 16:41:16 +0900

    회원가입

--- src/pages/sign/SignUpPage.tsx
+++ src/pages/sign/SignUpPage.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
-
+import Post from 'components/sign/Post';
 import 'styles/sign/signUp.css'
 
 import Form from 'react-bootstrap/Form';
@@ -34,10 +34,14 @@ interface Data {
 interface CheckData{
 	name: boolean,
 	nickName: boolean,
-	email:boolean
+	email: boolean,
+	authNumber:boolean,
 }
 
 const SignUpPage = () => {
+
+
+
 	const navigate = useNavigate();
 	const [cookies, setCookie, removeCookie] = useCookies(); 
 	const data: Data =
@@ -45,24 +49,28 @@ const SignUpPage = () => {
 		name: '', nickName: '', email: '', kakao: useLocation().state, memberInfoEntity: {
 			addressNum:"",addressLocation:"",addressDetail:"",phoneFirst:"",phoneMiddle:"",phoneLast:"",gender:"남",age:0
 		} }
-	const checkData: CheckData = {name:false,nickName:false,email:false}
+	const checkData: CheckData =
+		{ name: false, nickName: false, email: false ,authNumber:false}
 	const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+	let authNumber = "";
+	//팝업관련
+	const [enroll_company, setEnroll_company] = useState({
+	  code:'',
+    addr: '',
+	});
+	const [popup, setPopup] = useState(false);
+	const handleComplete = (data:any) => {
+			setPopup(!popup);
+	}
 
-	const check = useCallback(() => { 
-		if (!checkData.name) {
-			alert("이름을 정확하게 입력해주세요.");
-		} else if (!checkData.nickName) {
-			alert("닉네임 중복확인이 필요합니다.")
-		} else if (!checkData.email) {
-			alert("이메일을 정확하게 입력해주세요.");
-		} else {
-			signup();
-		}
+	const sendSMS = useCallback((e: any) => {//인증번호를 만들어서 문자로 보냅니다.
+		var phone = "" + data.memberInfoEntity.phoneFirst + data.memberInfoEntity.phoneMiddle + data.memberInfoEntity.phoneLast;
+		authNumber =  String(Math.floor(Math.random()*1000000)).padStart(6, "0");
+		axios.post("api/all/sendsms", { "phoneNumber": phone, "authNumber": authNumber }).then(() => { alert("인증번호 전송완료") }).catch(() => { alert("전송 실패") });
 	}, []);
-
+	//가입함수
 	const signup = useCallback(() => { 
-	
-		
+		alert("가입완료");
 		axios.post('/api/all/signup', data).then((res) => { //회원가입 정보를 서버로 보내서 디비에 저장한다.
 			axios({// 디비 저장에 성공했다면 카카오 아이디로 로그인을 실시 한다.
 				method: "GET",
@@ -84,21 +92,83 @@ const SignUpPage = () => {
               nickName: res.data.nickName,
               email:res.data.email
           }
-          
-         
           localStorage.setItem("userInfo",JSON.stringify(resData));
-          alert("로그인 성공")
           navigate('/');
-          
           })
 			}).catch((err) => { })
-				
-				
 		}).catch((err) => { alert('심각한 애러')})
 
 		
-	},[]);
+	}, []);
+	
+
+	//check	정확히 입력했는지 확인합니다.
+	const check = useCallback((e:any) => { 
+		const form = e.target.form;
+		if (!checkData.name) {
+			alert("이름을 정확하게 입력해주세요.");
+		} else if (data.memberInfoEntity.age == 0) {
+			alert("나이를 정확히 입력해주세요")
+		}	else if (!checkData.nickName) {
+			alert("닉네임 중복확인이 필요합니다.")
+		} else if (!checkData.email) {
+			alert("이메일을 정확하게 입력해주세요.");
+		} else if (!checkData.authNumber) {
+			alert("휴대폰 인증을 완료해주세요");
+		} else if (form.addressNum.value == '') {
+			alert("주소를 입력해주세요");
+		}
+		else {
+			data.memberInfoEntity.addressNum = form.addressNum.value;
+			data.memberInfoEntity.addressLocation = form.addressLocation.value;
+			signup();
+		}
+	}, []);
+
+	const checkNickName = useCallback((e: any) => { //닉네임 중복을 확인합니다.
+		if (data.nickName == '') {
+			checkData.nickName = false;
+			alert("사용 불가능 한 닉네임 입니다.");
+		} else {
+			axios.get('/api/all/signUp/checkNickName?nickName='+data.nickName).then((res)=>{
+				if (res.data) {
+					checkData.nickName = true;
+					alert("사용 가능 한 닉네임 입니다.");
+					
+				} else {
+					checkData.nickName = false;
+					alert("사용 불가능 한 닉네임 입니다.");
+					
+				}
+		})
+		}
+	}, []);
+	
+	const checkNumber = useCallback((e: any) => {//숫자만 입력했지 확인합니다.
+		e.target.value = e.target.value.replace(/[^0-9]/gi, "");
+	}, []);
+
+	const checkAuthNumber = useCallback((e: any) => {//인증번호 확인 버튼
+		const form = e.target.form;
+		const number = form.authNumber.value;
 		
+		
+		
+		if (number != '' && number == authNumber) {
+			alert("휴대폰 인증 성공");
+			const phoneInputs: any[] = Array.from(form.querySelectorAll(".phone"));//인증 성공이라면 입력정보 변경 못하도록 다 막는다.
+			for (var i = 0; i < phoneInputs.length; i++){
+				phoneInputs[i].disabled = true;
+			}
+			checkData.authNumber = true;
+			
+
+		} else {
+			alert("정확한 인증번호를 입력하세요")
+		}
+		
+	}, []);
+
 	const onChangeName = useCallback((e: any) => {
 		data.name = e.target.value;
 		if (e.target.value!='') {
@@ -107,6 +177,7 @@ const SignUpPage = () => {
 			checkData.name = false;
 		}
 	}, []);
+
 	const onChangeNickName =useCallback((e: any) => { 
 		data.nickName = e.target.value;
 		checkData.nickName = false;
@@ -116,26 +187,6 @@ const SignUpPage = () => {
 		data.email = e.target.value;
 		checkData.email = regex.test(e.target.value); 
 	},[]);
-		
-	const checkNickName = useCallback((e: any) => { 
-		axios.get('/api/all/signUp/checkNickName?nickName='+data.nickName).then((res)=>{
-			if (res.data) {
-				checkData.nickName = true;
-				alert("사용 가능 한 닉네임 입니다.");
-				
-			} else {
-				checkData.nickName = false;
-				alert("사용 불가능 한 닉네임 입니다.");
-				
-			}
-		})
-
-
-	},[]);
-		
-	const checkNumber = useCallback((e: any) => {
-		e.target.value = e.target.value.replace(/[^0-9]/gi, "");
-   },[])
 	const onChangeAge =  useCallback((e: any) => { 
 		data.memberInfoEntity.age = e.target.value;
 	}, []);
@@ -151,13 +202,7 @@ const SignUpPage = () => {
 	const onChangePhone3 =  useCallback((e: any) => { 
 		data.memberInfoEntity.phoneLast = e.target.value;
 	}, []);
-	const onChangeAddressNum =  useCallback((e: any) => { 
-		data.memberInfoEntity.addressNum = e.target.value;
-	}, []);
-	const onChangeAddressLocation =  useCallback((e: any) => { 
-		data.memberInfoEntity.addressLocation = e.target.value;
-	}, []);
-	const onChangeAddressDetail =  useCallback((e: any) => { 
+	const onChangeAddressDetail = useCallback((e: any) => { 
 		data.memberInfoEntity.addressDetail = e.target.value;
 	}, []);
 
@@ -167,7 +212,7 @@ const SignUpPage = () => {
 	return (
 		<div className='common_full'>
 			<Header/>
-			<div className='common_container'>
+			<form className='common_container'>
 				<div className='signUp_title'>
 					회원가입
 				</div>
@@ -231,40 +276,60 @@ const SignUpPage = () => {
 						<InputGroup size="sm" className="mb-3">
 							<InputGroup.Text id="inputGroup-sizing-sm">휴대폰</InputGroup.Text>
 							<Form.Control
+								className='phone'
 								aria-label="Small"
 								aria-describedby="inputGroup-sizing-sm"
 								onChange={onChangePhone1}
 							/>
 							<InputGroup.Text id="inputGroup-sizing-sm">-</InputGroup.Text>
 							<Form.Control
+								className='phone'
 								aria-label="Small"
 								aria-describedby="inputGroup-sizing-sm"
 								onChange={onChangePhone2}
 							/>
 							<InputGroup.Text id="inputGroup-sizing-sm">-</InputGroup.Text>
 							<Form.Control
+								className='phone'
 								aria-label="Small"
 								aria-describedby="inputGroup-sizing-sm"
 								onChange={onChangePhone3}
-								/>
+							/>
+							<Button className='phone' variant="dark" onClick={sendSMS}>인증번호 전송</Button>
 						</InputGroup>
 						<InputGroup size="sm" className="mb-3">
+							<InputGroup.Text id="inputGroup-sizing-sm">인증번호</InputGroup.Text>	
+							<Form.Control
+								className='phone'
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								name = "authNumber"
+							/>
+							<Button className='phone' variant="dark" onClick={checkAuthNumber}>인증번호 확인</Button>
+						</InputGroup>
+						<InputGroup size="sm" className="mb-3">
+							<InputGroup.Text id="inputGroup-sizing-sm">우편번호</InputGroup.Text>
+							<Form.Control
+								name = "addressNum"
+								aria-label="Small"
+								aria-describedby="inputGroup-sizing-sm"
+								value={enroll_company.code}
+								disabled
+							/>
 							<InputGroup.Text id="inputGroup-sizing-sm">주소</InputGroup.Text>
 							<Form.Control
+								name = "addressLocation"
 								aria-label="Small"
 								aria-describedby="inputGroup-sizing-sm"
-								onChange={onChangeAddressLocation}
-							/><InputGroup.Text id="inputGroup-sizing-sm">우편번호</InputGroup.Text>
-							<Form.Control
-								aria-label="Small"
-								aria-describedby="inputGroup-sizing-sm"
-								onChange={onChangeAddressNum}
+								value={enroll_company.addr}
+								disabled
 							/>
-							<Button variant="dark" >주소찾기</Button>
+							<Button variant="dark" onClick={handleComplete}>주소찾기</Button>
 						</InputGroup>
 							<InputGroup size="sm" className="mb-3">
 							<InputGroup.Text id="inputGroup-sizing-sm">상세주소</InputGroup.Text>
 							<Form.Control
+								name = "addressDetail"
 								aria-label="Small"
 								aria-describedby="inputGroup-sizing-sm"
 								onChange={onChangeAddressDetail}
@@ -272,7 +337,7 @@ const SignUpPage = () => {
 							
 						</InputGroup>
 
-
+					{popup && <Post company={enroll_company} setcompany={setEnroll_company}></Post>}
 						
 					</div>
 				</div>
@@ -280,7 +345,7 @@ const SignUpPage = () => {
 					<Button variant="dark" onClick={check}>회원가입</Button>
 				
 				</div>
-			</div>
+			</form>
 			<Footer/>
 		</div>
 	);
