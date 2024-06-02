7b216cf472e1edfe70214bbfc3fbdc2a5010df24
Yongmini <dev.yongmini@gmail.com>
2024-06-02 15:28:47 +0900

    feat. MoyeoraDetailPage add.

--- src/router/mainRouters/MoyeoraRouter.tsx
+++ src/router/mainRouters/MoyeoraRouter.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import CreateMoyeora from 'pages/moyeora/CreateMoyeoraPage';
+import MoyeroDetail from 'pages/moyeora/MoyeoraDetailPage';
 
 const MoyeoraRouter = () => {
   return (
     <Routes>
       <Route path="/createMoyeora" element={<CreateMoyeora/>} />
+      <Route path="/moyeroDetail" element={<MoyeroDetail/>} />
     </Routes>
   )
 }
