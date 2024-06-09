25224670ebb0a36b24c66ff7f0ab2d616bdb3093
Tae Hyeon Lee <harry7141@gmail.com>
2024-06-09 17:10:46 +0900

    123

--- src/components/myProfile/myProfileMenu/myProfileInfo/VisitorBook.tsx
+++ src/components/myProfile/myProfileMenu/myProfileInfo/VisitorBook.tsx
@@ -5,17 +5,20 @@ import 'styles/myProfile/myProfileInfo/visitorBook.css'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import { ProfileConfig } from 'store/slices/profileConfigSlice';
 import axios from 'axios';
-import { Pagination } from 'react-bootstrap';
+import { FormControlProps, Pagination } from 'react-bootstrap';
 import { FaStar } from "react-icons/fa6";
 import { CiStar } from "react-icons/ci";
 import { IconContext } from 'react-icons';
-
-interface Review {
-
+import { useCookies } from 'react-cookie';
+import { authAxios } from 'utils/auth/authAxios';
+import { authException } from 'utils/auth/authException';
+interface ReviewType {
+	ownerNickName: String;
 	writerNickName: String;
 	star: number;
 	content: String;
@@ -23,20 +26,21 @@ interface Review {
 }
 
 const VisitorBook = () => {
-		const profileConfig:ProfileConfig= useSelector((state: RootState) => {
-    	return state.profileConfig
-		});
-	
-	const [review, setReview] = useState<Review[] | null>(null);
-	const [pageNums, setPageNums] = useState<React.JSX.Element[]|null>();
 	const fristSize = 3;//더보기 누르기 전 사이즈
-	const pagingSize = 5;//더보기 누른 후 사이즈
+	const pagingSize = 7;//더보기 누른 후 사이즈
+	const profileConfig:ProfileConfig= useSelector((state: RootState) => {
+    	return state.profileConfig
+	});
+	const [cookies, setCookie, removeCookie] = useCookies();
+	const [review, setReview] = useState<ReviewType[] | null>(null);
+	const [pageNums, setPageNums] = useState<React.JSX.Element[]|null>();
+
 	const star: any[] = [<FaStar />, <FaStar />, <FaStar />, <FaStar />, <FaStar />]
 
 	const [starPoint, setStarPoint] = useState(3);
 	const [fullStar,setFullStar]=useState<React.JSX.Element[]>([]);
 	const [emtyStar,setEmtyStar]=useState<React.JSX.Element[]>([]);
-
+	const [content, setContent] = useState<String>("");
 
 	const handleStar = () => { //최대페이지로 화면에 출력할 페이지 숫자를 만들어줌
 	
@@ -67,32 +71,36 @@ const VisitorBook = () => {
 		)
 			.then((response) => {
 			
-				setReview(response.data as Review[])
+				setReview(response.data as ReviewType[])
 			})
       .catch((error) => console.log(error));
    
     }, [profileConfig]);
 	
-	const moreList = useCallback((e: any) => {//더보기 눌렀을때
+	const clickMoreList = useCallback((e: any) => {//더보기 눌렀을때
 		e.target.hidden = true;
+		morelist();
+	}, [profileConfig]);
 	
+	const morelist = useCallback(() => {//더보기
 		axios.get('/api/all/memberReviewList?page=0&size='+pagingSize+'&nickName='+profileConfig.nickName)
 			.then((response) => {
 				setReview(
-					response.data as Review[]
+					response.data as ReviewType[]
 				)
 				handlePage(1, response.data[0].totalPage);
 			})
       .catch((error) => console.log(error));
    
-    }, [profileConfig]);
+		}, [profileConfig]);
+
 
 	const clickPageNum = (number:number) => {//페이지 숫자 클릭시
 		axios.get('/api/all/memberReviewList?page='+(number-1)+'&size='+pagingSize+'&nickName='+profileConfig.nickName)
 			.then((response) => {
 				console.log(response.data)
 				setReview(
-					response.data as Review[]
+					response.data as ReviewType[]
 				)
 				handlePage(number, response.data[0].totalPage);
 			})
@@ -111,7 +119,33 @@ const VisitorBook = () => {
 		setPageNums(items);
 	}
 	
-	
+	const changeContent = (e: any) => {
+		let content = e.target.value;
+		setContent(content);
+	}
+	const clickWrite = (e: any) => {
+		if (content == "") {
+			alert('내용을 입력해주세요')
+		} else {
+			let sendData: ReviewType = {
+				ownerNickName: profileConfig.nickName,
+				writerNickName: "",
+				star: starPoint,
+				content: content,
+				date: "",
+			}
+			authAxios.post("/api/user/insertReview", sendData).then((res) => {
+				if (authException(res, [cookies, setCookie, removeCookie])) {
+					morelist();
+					e.target.form.content.value = "";
+				}
+			}
+			
+			).catch(()=>alert('로그인후 이용가능합니다.'))
+
+
+		}
+	}
 	return (
 		
 		<div className='visitorBook_full'>
@@ -138,7 +172,7 @@ const VisitorBook = () => {
 						
 						}
 						<div className='visitorBook_list_button'>
-						<Button variant="outline-dark" size="lg" onClick={moreList}>
+						<Button variant="outline-dark" size="lg" onClick={clickMoreList}>
 							
 							더보기 +
 							
@@ -153,20 +187,22 @@ const VisitorBook = () => {
 						</div>
 						<div className='visitorBook_comment_write'>
 							
-							<FloatingLabel controlId="floatingTextarea2" label="">
-								<IconContext.Provider value={{ style: { fontSize:'25px',cursor:"pointer" } }}>
+							
+								<IconContext.Provider value={{ style: { fontSize:'24px',cursor:"pointer" } }}>
 								{fullStar.map((stars, idx) => { if (idx < starPoint) return stars })}
 								{emtyStar.map((stars, idx) => { if (starPoint - 1 < idx && idx < 5) return stars })}
 								</IconContext.Provider>
 								<Form.Control
+									name = "content"
 									as="textarea"
-									placeholder="Leave a comment here"
+									placeholder="내용 입력"
 									style={{ height: '150px' }}
+									onKeyUp={changeContent}
 								/>
-							</FloatingLabel>
+							
 						</div>
 						<div className='visitorBook_comment_button'>
-							<Button variant="outline-dark" size="lg">
+							<Button variant="outline-dark" size="lg" onClick={clickWrite}>
 								등록하기
 							</Button>
 						</div>
