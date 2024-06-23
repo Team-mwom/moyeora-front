8094fb5be976be0865bb632d40b39b43ca27c908
Tae Hyeon Lee <harry7141@gmail.com>
2024-06-23 14:13:23 +0900

    이미지

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,32 +8,21 @@ import ProfileRouter from 'router/mainRouters/ProfileRouter';
 import SignRouter from 'router/mainRouters/SignRouter';
 import TestRouter from 'router/testRouters/TestRouter';
 import MoyeoraRouter from 'router/mainRouters/MoyeoraRouter';
-
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        
         {getRoutes(MainRouter)}
         {getRoutes(ProfileRouter)}
         {getRoutes(SignRouter)}
         {getRoutes(TestRouter)}
         {getRoutes(MoyeoraRouter)}
-
-
-
-
-
-
-      
-        
         <Route path="*" element={<PageNotFound />} /> {/*이친구는 맨밑에 위치 */}
       </Routes>
     </BrowserRouter>
     
   )
 }
-
 const getRoutes = (routes:any) => {
   return routes.map((route: any, index: any) => {
       return (
--- src/router/mainRouters/MoyeoraRouter.tsx
+++ src/router/mainRouters/MoyeoraRouter.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-
-import {Routes, Route} from 'react-router-dom';
-
 import CreateMoyeora from 'pages/moyeora/CreateMoyeoraPage';
 import MoyeroDetail from 'pages/moyeora/MoyeoraDetailPage';
-
 
 const MoyeoraRouter = [
   {path: '/createMoyeora', element:< CreateMoyeora />},
