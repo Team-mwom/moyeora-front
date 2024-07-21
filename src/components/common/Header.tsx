e96d863ccae76b0ed8d30a0ccd69bc24856b5e09
Tae Hyeon Lee <harry7141@gmail.com>
2024-07-21 17:00:43 +0900

    header 로그인 정보

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,32 +1,45 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import 'styles/common/components/header.css'
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import axios from 'axios';
+import ProfileImgNick from './profile/ProfileImgNick';
+
+const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+
 
 const Header = () => {
+
 	const userInfo = JSON.parse(localStorage.getItem("userInfo") as string);
 	const navigate = useNavigate();
-
   const [cookies, setCookie, removeCookie] = useCookies();
-
-	const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
-	const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
-	const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+	const [profileDetailOpen, setProfileDetailOpen] = useState<boolean>(false);
 
 	const linkSignIn = () => {
+		if(userInfo == null)
 		window.location.href = KAKAO_AUTH_URL
 	}
 
 	const linkHome = useCallback(
-		(e:React.MouseEvent<HTMLElement>) => {
+		(e: React.MouseEvent<HTMLElement>) => {
+			setProfileDetailOpen(false);
 			navigate("/");
 	}
-	, []);
-
+		, []);
+	const linkProfile = useCallback(
+		(e: React.MouseEvent<HTMLElement>) => {
+			setProfileDetailOpen(false);
+			navigate("/profile/"+userInfo.nickName);
+	}
+		, [userInfo]);
+	
 	const signOut = useCallback(
 		(e: React.MouseEvent<HTMLElement>) => {
+			setProfileDetailOpen(false);
 			axios.get("/api/all/signOut").then(() => { 
 				localStorage.clear();
         removeCookie('refreshToken', {path:'/'});
@@ -35,6 +48,10 @@ const Header = () => {
       
     }
 		, []);
+	const profileDetailHandle = (a: any) => () => {
+		if (userInfo != null)
+			setProfileDetailOpen(a);
+	}
 
 	return (
 		<div className='header_full'>
@@ -47,17 +64,45 @@ const Header = () => {
 						<div className='logo_inner'>
 							<img className="logo" alt="logo_message" src="/images/logo_message.png" />
 						</div>
-					<div className='logo_inner'>
+					<div className='logo_inner logo_inner_profile' onMouseEnter={profileDetailHandle(true)} onMouseLeave={profileDetailHandle(false)} onClick={linkSignIn}>
 						{userInfo == null ?
 							//로그인 상태 아닐때
-							<div className='signIn' onClick={linkSignIn}>
+							<div className='signIn' >
 								로그인 / 회원가입
 							</div>
 							:
 							//로그인 상태 일때
-							// <img className="logo" alt="logo_account_info" src="/images/logo_account_info.png" />
-							<div className='signIn' onClick={signOut}>이름:{userInfo.name},닉네임:{userInfo.nickName},이메일:{userInfo.email}</div>
+							<ProfileImgNick
+												nick={userInfo.nickName}
+												size={40}
+												distance={10}
+												fontSize={19}
+							/>
+							//<div className='signIn' onClick={signOut}>이름:{userInfo.name},닉네임:{userInfo.nickName},이메일:{userInfo.email}</div>
 						}
+						<br></br>
+						{profileDetailOpen ?
+							<div className='profileDetail_contailer'>
+								<div className='profileDetail_inner' onClick={linkProfile}>
+									프로필
+								</div>
+								<div className='profileDetail_inner' onClick={linkProfile}>
+									내 모여라
+								</div>
+								<div className='profileDetail_inner' onClick={linkProfile}>
+									캘린더
+								</div>
+								<div className='profileDetail_inner' onClick={linkProfile}>
+									설정
+								</div>
+								<div className='profileDetail_inner profileDetail_logout' onClick={signOut}>
+									로그아웃
+								</div>
+							</div>
+							: ""}
+
+
+
 						</div>
 					</div>
 			</div>
