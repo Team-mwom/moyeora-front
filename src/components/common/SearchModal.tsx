e9236b925967f18b4767d3160c3608789c862f8c
JIN-RYEOL <0205kjl@naver.com>
2024-08-25 15:50:48 +0900

    검색 필터링 진행중

--- src/components/common/SearchModal.tsx
+++ src/components/common/SearchModal.tsx
@@ -70,13 +70,22 @@ const SearchModal: React.FC<SearchModalProps> = ({ isOpen, onClose, handleFilter
           >
             <option value="">선택해주세요</option>
             <option value="서울">서울</option>
-            <option value="경기">경기</option>
-            <option value="인천">인천</option>
             <option value="부산">부산</option>
             <option value="대구">대구</option>
+            <option value="인천">인천</option>
             <option value="광주">광주</option>
             <option value="대전">대전</option>
             <option value="울산">울산</option>
+            <option value="세종">세종</option>
+            <option value="경기">경기</option>
+            <option value="강원">강원</option>
+            <option value="충청북도">충북</option>
+            <option value="충청남도">충남</option>
+            <option value="전라북도">전북</option>
+            <option value="전라남도">전남</option>
+            <option value="경상북도">경북</option>
+            <option value="경상남도">경남</option>
+            <option value="제주">제주</option>
           </select>
         </div>
 
